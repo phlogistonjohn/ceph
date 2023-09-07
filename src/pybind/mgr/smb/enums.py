0a76e63f60d359fc614fd84c4164b453e8d9eda6
John Mulligan <jmulligan@redhat.com>
2023-09-07 18:50:40 -0400

    xxx: add enums.py
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ src/pybind/mgr/smb/enums.py
@@ -0,0 +1,51 @@
+import sys
+
+
+if sys.version_info >= (3, 11):  # pragma: no cover
+    from enum import StrEnum as _StrEnum
+else:  # pragma: no cover
+    import enum
+
+    # work like StrEnum for older python versions for our purposes
+    class _StrEnum(str, enum.Enum):
+        def __str__(self) -> str:
+            return self.value
+
+
+class CephFSStorageProvider(_StrEnum):
+    KERNEL_MOUNT = 'kcephfs'
+    SAMBA_VFS = 'samba-vfs'
+
+
+class SubSystem(_StrEnum):
+    CEPHFS = 'cephfs'
+
+
+class Intent(_StrEnum):
+    PRESENT = 'present'
+    REMOVED = 'removed'
+
+
+class State(_StrEnum):
+    CREATED = 'created'
+    NOT_PRESENT = 'not present'
+    PRESENT = 'present'
+    REMOVED = 'removed'
+    UPDATED = 'updated'
+
+
+class AuthMode(_StrEnum):
+    USER = 'user'
+    ACTIVE_DIRECTORY = 'active-directory'
+
+
+class JoinSourceType(_StrEnum):
+    PASSWORD = 'password'
+    HTTP_URI = 'http_uri'
+    RESOURCE = 'resource'
+
+
+class UserGroupSourceType(_StrEnum):
+    INLINE = 'inline'
+    HTTP_URI = 'http_uri'
+    RESOURCE = 'resource'
