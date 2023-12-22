cb55d78e9906c20c805cc5ec4b12aa74b7e3be7a
John Mulligan <jmulligan@redhat.com>
2023-12-22 10:48:17 -0500

    xxx: new resourcelib
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ src/pybind/mgr/smb/resourcelib.py
@@ -0,0 +1,393 @@
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+    TypeVar,
+    Type,
+    Tuple,
+    Generic,
+    Annotated,
+)
+import dataclasses
+import enum
+
+from .proto import Simplified
+
+T = TypeVar('T')
+
+
+try:  # pragma: no cover
+    from typing import get_args
+except ImportError:  # pragma: no cover
+
+    def get_args(t: Any) -> Tuple:
+        try:
+            return t.__args__
+        except AttributeError:
+            return tuple()
+
+
+class ResourceError(Exception):
+    pass
+
+
+class MissingFieldError(KeyError, ResourceError):
+    pass
+
+
+@dataclasses.dataclass
+class Extras:
+    # embedded: combine the data for an object with that of its parent
+    embedded: bool = False
+    # keep_none: if true, none objects will appear in the simple data output.
+    # if false, the value and key will be elided
+    keep_none: bool = False
+    # keep_false: if true, "falsy" objects will appear in the simple data output.
+    # if false, the value and key will be elided
+    keep_false: bool = True
+    #
+    alt_keys: Optional[List[str]] = None
+
+
+class ResourceType:
+    """A read-only property of a class acting as a resource type. When accessed,
+    returns the name of the resource type.
+    """
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __get__(self, obj: Any, objtype: Any = None) -> str:
+        return self.name
+
+
+_RESOURCES: Dict[str, Type[T]] = {}
+
+
+def to_simplified(obj: T) -> Simplified:
+    return _to_simplified(_typeinfo(type(obj)), obj)
+
+
+def from_simplified(cls: Type[T], data: Simplified) -> T:
+    return _from_object(_typeinfo(cls), _Source(data))
+
+
+def resource(
+    resource_name: str, registry: Optional[Dict[str, Type[T]]] = None
+) -> Type[T]:
+    assert resource_name
+    decf = _resourceclass(resource_name)
+
+    def _decorator(cls: Any) -> Any:
+        cls = decf(cls)
+        nonlocal registry
+        if registry is None:
+            registry = _RESOURCES
+        if resource_name in registry:
+            raise KeyError(f'{resource_name} already used')
+        registry[resource_name] = cls
+        return cls
+
+    return _decorator
+
+
+def component():
+    return _resourceclass('')
+
+
+def get_resource(
+    resource_name, registry: Optional[Dict[str, Type[T]]] = None
+) -> Type[T]:
+    if registry is None:
+        registry = _RESOURCES
+    try:
+        return registry[resource_name]
+    except KeyError:
+        raise ValueError(f'no matching resource_type: {resource_name}')
+
+
+def load(data: Simplified) -> Any:
+    print("LOADD", data)
+    if 'resource_type' not in data and 'resources' in data:
+        res = data['resources']
+        if not isinstance(res, list):
+            raise TypeError(res)
+        out = []
+        for item in res:
+            out.extend(load(item))
+        return out
+    if 'resource_type' not in data:
+        raise ValueError('no resource_type')
+    rcls = get_resource(data['resource_type'])
+    return [rcls.from_simplified(data)]
+
+
+###############################################################
+
+
+def _resourceclass(resource_name: str):
+    def _decorator(cls: Any) -> Any:
+        cls = dataclasses.dataclass(cls)
+        if resource_name:
+            cls.resource_type = ResourceType(resource_name)
+        cls._conversions = {}
+        if getattr(cls, 'to_simplified', None) is None:
+            cls.to_simplified = to_simplified
+        if getattr(cls, 'from_simplified', None) is None:
+            cls.from_simplified = classmethod(from_simplified)
+            cls._from_simplfied_object = classmethod(_from_cls_object)
+        return cls
+
+    return _decorator
+
+
+class _typeinfo:
+    def __init__(self, target_type: Any) -> None:
+        print("TYPEINFO", target_type)
+        self._meta = getattr(target_type, '__metadata__', None)
+        # if the target type is Annotated we just want the metadata (our
+        # cusomization params) and the underlying type.
+        if self._meta:
+            target_type = target_type.__origin__
+        self.target_type = target_type
+        self._args = getattr(self.target_type, '__args__', None)
+        self._origin = getattr(self.target_type, '__origin__', None)
+
+    def is_optional(self) -> bool:
+        if self._args is None:
+            return False
+        return any(t is type(None) for t in self._args)
+
+    def extras(self) -> Extras:
+        meta = self._meta or []
+        try:
+            return [a for a in meta if isinstance(a, Extras)][0]
+        except IndexError:
+            return Extras()
+
+    def unwrap(self) -> '_typeinfo':
+        if self._args is None:
+            return self
+        nnt = [t for t in self._args if t is not type(None)][0]
+        return self.__class__(nnt)
+
+    def unwrap_optional(self) -> '_typeinfo':
+        if not self.is_optional():
+            return self
+        return self.unwrap()
+
+    def unwrap_dict(self) -> Tuple['_typeinfo', '_typeinfo']:
+        assert self._args and len(self._args) == 2
+        kt, vt = self._args
+        return self.__class__(kt), self.__class__(vt)
+
+    def takes(self, *target_types: Any) -> bool:
+        if self._origin:
+            return self._origin in target_types
+        return self.target_type in target_types
+
+    def __repr__(self) -> str:
+        return f'_typeinfo({self.target_type!r})'
+
+
+def _to_simplified(type_info: _typeinfo, obj: Any) -> Simplified:
+    result: Simplified = {}
+    rtype = getattr(obj, 'resource_type', None)
+    if rtype is not None:
+        result['resource_type'] = rtype
+    for fld in dataclasses.fields(obj):
+        type_info = _typeinfo(fld.type)
+        sv = _simplify_field(_typeinfo(fld.type), getattr(obj, fld.name))
+        extras = type_info.extras()
+        if extras.embedded and isinstance(sv, dict):
+            result.update(sv)
+        elif (
+            sv
+            or (sv is None and extras.keep_none)
+            or (sv is not None and not sv and extras.keep_false)
+        ):
+            result[fld.name] = sv
+    return result
+
+
+def _simplify_field(type_info: _typeinfo, obj: Any) -> Simplified:
+    ts = getattr(obj, 'to_simplified', None)
+    if ts:
+        return ts()
+    if isinstance(obj, list):
+        assert type_info.unwrap_optional().takes(list, List)
+        childttype = type_info.unwrap_optional().unwrap()
+        return [_simplify_field(childttype, v) for v in obj]
+    if isinstance(obj, dict):
+        assert type_info.unwrap_optional().takes(dict, Dict)
+        kt, vt = type_info.unwrap_optional().unwrap_dict()
+        return {
+            _simplify_field(kt, k): _simplify_field(vt, v)
+            for k, v in obj.items()
+        }
+    if isinstance(obj, str):
+        return str(obj)
+    if isinstance(obj, (int, float)):
+        return obj
+    if obj is None:
+        assert type_info.is_optional()
+        return None
+    raise TypeError(obj)
+
+
+_nodefault = object()
+
+
+class _Source:
+    def __init__(self, data: Simplified) -> None:
+        self.data = data
+
+    def __getitem__(self, key) -> '_Source':
+        return self.__class__(self.data[key])
+
+    def get(self, *keys: str, default=_nodefault) -> '_Source':
+        for key in keys:
+            try:
+                return self[key]
+            except KeyError:
+                pass
+        if default is _nodefault:
+            raise KeyError(key)
+        return self.__class__(default)
+
+    def get_or_borrow(self, *keys: str) -> '_Source':
+        try:
+            return self.get(*keys)
+        except KeyError:
+            return self.borrow()
+
+    def borrow(self) -> '_Source':
+        return self
+
+    def contents(self) -> List['_Source']:
+        return [self.__class__(v) for v in self.data]
+
+    def dict_contents(self) -> List[Tuple['_Source', '_Source']]:
+        return [
+            (self.__class__(k), self.__class__(v))
+            for k, v in self.data.items()
+        ]
+
+    def __repr__(self) -> str:
+        return f'<_Source({self.data!r}) at {id(self):0x}>'
+
+
+def _xt(f):
+    def _func(*args, **kwargs):
+        print(f'\ncall: {f}', args, kwargs)
+        return f(*args, **kwargs)
+
+    return _func
+
+
+@_xt
+def _from_cls_object(cls: Any, source: _Source) -> Any:
+    return _from_object(_typeinfo(cls), source)
+
+
+@_xt
+def _from_object(type_info: _typeinfo, source: _Source) -> Any:
+    try:
+        fields = dataclasses.fields(type_info.target_type)
+        _assert_resource_type(type_info, source)
+    except TypeError:
+        return _from_scalar(type_info, source)
+    kw = {}
+    for fld in fields:
+        print("from object fld-->", fld.name)
+        try:
+            kw[fld.name] = _from_object_field(
+                _typeinfo(fld.type), source, fld.name
+            )
+        except MissingFieldError:
+            pass
+    print("NEW", type_info.target_type, '(', kw, ')')
+    obj = type_info.target_type(**kw)
+    validate = getattr(obj, 'validate', None)
+    if validate:
+        validate()
+    return obj
+
+
+@_xt
+def _from_object_field(
+    type_info: _typeinfo, source: _Source, fname: str
+) -> Any:
+    is_optional = type_info.is_optional()
+    extras = type_info.extras()
+    innert = type_info.unwrap_optional()
+    if extras.embedded:
+        tgt = source.get_or_borrow(fname, *(extras.alt_keys or []))
+    else:
+        kw = {'default': None} if is_optional else {}
+        try:
+            tgt = source.get(fname, *(extras.alt_keys or []), **kw)
+        except KeyError as err:
+            raise MissingFieldError(fname) from err
+    print('field data CHOSE:', tgt)
+
+    if tgt.data is None and is_optional:
+        return None
+
+    _fso = getattr(innert.target_type, '_from_simplfied_object', None)
+    _fss = getattr(innert.target_type, 'from_simplified', None)
+    print('__methods__>', _fso, _fss)
+    if _fso and source.data is not None:
+        # _from_simplfied_object is semi-private and supports the direct use
+        # of a source
+        return _fso(tgt)
+    if _fss and source.data is not None:
+        # from_simplified is the fully public version and doesn't know anything
+        # about our special source type
+        return _fss(tgt.data)
+    if innert.takes(list, List):
+        assert isinstance(tgt.data, list)
+        return [_from_object(innert.unwrap(), sv) for sv in tgt.contents()]
+    if innert.takes(dict, Dict):
+        assert isinstance(tgt.data, dict)
+        kt, vt = innert.unwrap_dict()
+        return {
+            _from_scalar(kt, sk): _from_object(vt, sv)
+            for sk, sv in tgt.dict_contents()
+        }
+    try:
+        return _from_scalar(innert, tgt, optional=is_optional)
+    except TypeError:
+        raise ValueError(f'field {fname} missing or invalid type')
+
+
+@_xt
+def _from_scalar(
+    type_info: _typeinfo, source: _Source, optional: bool = False
+) -> Any:
+    value = source.data
+    if value is None and (optional or type_info.is_optional()):
+        return None
+    if issubclass(type_info.target_type, enum.Enum):
+        return type_info.target_type(tgt.data)
+    if isinstance(value, (str, int, float)):
+        return value
+    raise TypeError(value)
+
+
+@_xt
+def _assert_resource_type(type_info: _typeinfo, source: _Source) -> None:
+    expected_rt = getattr(type_info.target_type, 'resource_type', None)
+    if not expected_rt:
+        return
+    print('resource_type=', expected_rt)
+    try:
+        curr_resource_type = source['resource_type'].data
+    except KeyError:
+        raise ValueError('source object lacks resource type')
+    if expected_rt != curr_resource_type:
+        raise ValueError(
+            'resource type mismatch:'
+            f' expected {expected_rt!r}, got {curr_resource_type!r}'
+        )
---
+++ src/pybind/mgr/smb/tests/test_resourcelib.py
@@ -0,0 +1,379 @@
+from typing import Optional, List, Dict
+
+import dataclasses
+
+import pytest
+
+import smb.resourcelib as rlib
+
+
+@rlib.component()
+class tclass1:
+    foo: str
+    bar: int
+    baz: rlib.Annotated[str, rlib.Extras(keep_false=False)]
+    quux: Optional[List[int]] = None
+    womble: rlib.Annotated[Optional[int], rlib.Extras(keep_none=True)] = None
+    tags: Optional[Dict[str, int]] = None
+
+
+@rlib.component()
+class tclass2:
+    name: str
+    xyz: tclass1
+
+
+@rlib.component()
+class tclass3:
+    artists: List[str]
+    kind: str = ''
+
+
+@rlib.resource('test.res.one')
+class ResOne:
+    zim: str
+    zam: Optional[str] = None
+    jam: Optional[tclass2] = None
+
+
+@rlib.resource('test.res.two')
+class ResTwo:
+    aaa: str
+    bbb: List[tclass1]
+    ccc: Dict[str, str]
+    ddd: rlib.Annotated[tclass3, rlib.Extras(embedded=True)]
+
+
+def test_typeinfo():
+    fmap = {
+        f.name: rlib._typeinfo(f.type) for f in dataclasses.fields(tclass1)
+    }
+    assert 'foo' in fmap
+    assert 'bar' in fmap
+    assert 'baz' in fmap
+
+    assert not fmap['foo'].is_optional()
+    assert not fmap['bar'].is_optional()
+    assert not fmap['baz'].is_optional()
+    assert fmap['quux'].is_optional()
+    assert fmap['womble'].is_optional()
+
+    assert fmap['foo'].extras().keep_false == True
+    assert fmap['baz'].extras().keep_false == False
+    assert fmap['bar'].extras().keep_none == False
+    assert fmap['womble'].extras().keep_none == True
+
+    assert fmap['foo'].unwrap_optional() is fmap['foo']
+    assert fmap['quux'].unwrap_optional() is not fmap['foo']
+    assert fmap['quux'].unwrap_optional().takes(list, List)
+    assert fmap['quux'].unwrap_optional().unwrap_optional().takes(list, List)
+    assert fmap['baz'].unwrap_optional() is fmap['baz']
+
+    assert fmap['quux'].unwrap().takes(list, List)
+    assert fmap['quux'].unwrap().unwrap().takes(int)
+    assert fmap['foo'].unwrap() is fmap['foo']
+
+    assert fmap['tags'].unwrap_optional().takes(dict, Dict)
+    tkt, tvt = fmap['tags'].unwrap_optional().unwrap_dict()
+    assert tkt.takes(str)
+    assert tvt.takes(int)
+
+    assert '_typeinfo' in f'{fmap["foo"]}'
+
+
+def test_get_resource():
+    r1 = rlib.get_resource('test.res.one')
+    assert r1 is ResOne
+
+    with pytest.raises(ValueError, match='foo.bar'):
+        rlib.get_resource('foo.bar')
+
+
+def test_resouce_type_name_reuse_blocked():
+    with pytest.raises(KeyError):
+
+        @rlib.resource('test.res.one')
+        class FakeOne:
+            zzz: int
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        # basic use of a single non-nesting resource component
+        {
+            'obj': tclass1(
+                foo='okthen',
+                bar='chocolate',
+                baz='guitar',
+                womble=77,
+                tags={'red': 2, 'green': 6},
+            ),
+            'expected': {
+                'foo': 'okthen',
+                'bar': 'chocolate',
+                'baz': 'guitar',
+                'womble': 77,
+                'tags': {
+                    'red': 2,
+                    'green': 6,
+                },
+            },
+        },
+        # test simplifying a resource object
+        {
+            'obj': ResOne(
+                zim='yes',
+                jam=tclass2(
+                    name='nothing',
+                    xyz=tclass1(
+                        foo='',
+                        bar='chocolate',
+                        baz='',
+                        tags={'red': 2, 'green': 6},
+                    ),
+                ),
+            ),
+            'expected': {
+                'resource_type': 'test.res.one',
+                'zim': 'yes',
+                'jam': {
+                    'name': 'nothing',
+                    'xyz': {
+                        'foo': '',
+                        'bar': 'chocolate',
+                        'womble': None,
+                        'tags': {
+                            'red': 2,
+                            'green': 6,
+                        },
+                    },
+                },
+            },
+        },
+        # a resource object that does some embedding
+        {
+            'obj': ResTwo(
+                aaa='hello',
+                bbb=[tclass1(foo='child', bar='', baz='', womble=88)],
+                ccc={},
+                ddd=tclass3(['da Vinci', 'Picasso'], 'painting'),
+            ),
+            'expected': {
+                'resource_type': 'test.res.two',
+                'aaa': 'hello',
+                'bbb': [
+                    {'foo': 'child', 'bar': '', 'womble': 88},
+                ],
+                'ccc': {},
+                'artists': ['da Vinci', 'Picasso'],
+                'kind': 'painting',
+            },
+        },
+    ],
+)
+def test_to_simplified(params):
+    obj = params.get('obj')
+    sdata = obj.to_simplified()
+    assert params['expected'] == sdata
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        # round trip a simple single-level type
+        {
+            'target_type': tclass1,
+            'data': {
+                'foo': 'zowie',
+                'bar': 12,
+                'baz': 'fickle',
+            },
+            'expected': {
+                'foo': 'zowie',
+                'bar': 12,
+                'baz': 'fickle',
+                'womble': None,
+            },
+        },
+        # again, but with different values
+        {
+            'target_type': tclass1,
+            'data': {
+                'foo': 'howdy',
+                'bar': 22,
+                'baz': 'zombo',
+                'quux': [1, 9, 88],
+                'womble': 909,
+                'tags': {'eggs': 400, 'beans': 200},
+            },
+            'expected': {
+                'foo': 'howdy',
+                'bar': 22,
+                'baz': 'zombo',
+                'quux': [1, 9, 88],
+                'womble': 909,
+                'tags': {'eggs': 400, 'beans': 200},
+            },
+        },
+        # one level of nesting
+        {
+            'target_type': tclass2,
+            'data': {
+                'name': 'game',
+                'xyz': {
+                    'foo': 'within',
+                    'bar': 32,
+                    'baz': '',
+                    'quux': [3, 33, 333],
+                    'womble': 919,
+                    'tags': {},
+                },
+            },
+            'expected': {
+                'name': 'game',
+                'xyz': {
+                    'foo': 'within',
+                    'bar': 32,
+                    'quux': [3, 33, 333],
+                    'womble': 919,
+                    'tags': {},
+                },
+            },
+        },
+        # three possible levels, but keep it minimal
+        {
+            'target_type': ResOne,
+            'data': {
+                'resource_type': 'test.res.one',
+                'zim': 'zoom',
+            },
+            'expected': {
+                'resource_type': 'test.res.one',
+                'zim': 'zoom',
+            },
+        },
+        # three levels of nesting
+        {
+            'target_type': ResOne,
+            'data': {
+                'resource_type': 'test.res.one',
+                'zim': 'zoom',
+                'zam': 'greetings',
+                'jam': {
+                    'name': 'game',
+                    'xyz': {
+                        'foo': 'is here',
+                        'bar': 56,
+                        'baz': 'babble',
+                        'quux': [4, 0, 5],
+                        'womble': 92,
+                        'tags': {'bug': 9, 'feature': 4},
+                    },
+                },
+            },
+            'expected': {
+                'resource_type': 'test.res.one',
+                'zim': 'zoom',
+                'zam': 'greetings',
+                'jam': {
+                    'name': 'game',
+                    'xyz': {
+                        'foo': 'is here',
+                        'bar': 56,
+                        'baz': 'babble',
+                        'quux': [4, 0, 5],
+                        'womble': 92,
+                        'tags': {'bug': 9, 'feature': 4},
+                    },
+                },
+            },
+        },
+        # nesting with embedding
+        {
+            'target_type': ResTwo,
+            'data': {
+                'resource_type': 'test.res.two',
+                'aaa': 'apple',
+                'bbb': [
+                    {'foo': 'item1', 'bar': 'yes', 'baz': '0'},
+                    {'foo': 'item2', 'bar': 'no', 'baz': '10'},
+                ],
+                'ccc': {
+                    'cow': 'moo',
+                    'dog': 'arf',
+                },
+                'artists': ['me', 'you'],
+                'kind': 'very',
+            },
+            'expected': {
+                'resource_type': 'test.res.two',
+                'aaa': 'apple',
+                'bbb': [
+                    {
+                        'foo': 'item1',
+                        'bar': 'yes',
+                        'baz': '0',
+                        'womble': None,
+                    },
+                    {
+                        'foo': 'item2',
+                        'bar': 'no',
+                        'baz': '10',
+                        'womble': None,
+                    },
+                ],
+                'ccc': {
+                    'cow': 'moo',
+                    'dog': 'arf',
+                },
+                'artists': ['me', 'you'],
+                'kind': 'very',
+            },
+        },
+        #  embedding is optional in a source
+        {
+            'target_type': ResTwo,
+            'data': {
+                'resource_type': 'test.res.two',
+                'aaa': 'artichoke',
+                'bbb': [
+                    {'foo': 'item1', 'bar': 'yes', 'baz': '0'},
+                ],
+                'ccc': {
+                    'cow': 'moo',
+                    'dog': 'arf',
+                },
+                'ddd': {
+                    'artists': ['city', 'country'],
+                },
+            },
+            'expected': {
+                'resource_type': 'test.res.two',
+                'aaa': 'artichoke',
+                'bbb': [
+                    {
+                        'foo': 'item1',
+                        'bar': 'yes',
+                        'baz': '0',
+                        'womble': None,
+                    },
+                ],
+                'ccc': {
+                    'cow': 'moo',
+                    'dog': 'arf',
+                },
+                'artists': ['city', 'country'],
+                'kind': '',
+            },
+        },
+    ],
+)
+def test_from_simplified(params):
+    data = params.get('data')
+    typ = params.get('target_type')
+    obj = typ.from_simplified(data)
+    # test round tripping because asserting equality on the
+    # objects is not simple
+    sdata = obj.to_simplified()
+    assert params['expected'] == sdata
