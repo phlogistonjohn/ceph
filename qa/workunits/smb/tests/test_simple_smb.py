1431d058d2e5e45864750b42613d7ae0e6e42809
John Mulligan <jmulligan@redhat.com>
2025-11-19 16:29:37 -0500

    xxx: hax: qa/workunits/smb/tests/test_simple_smb.py
    Add dummy mgr interaction tests.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- qa/workunits/smb/tests/test_simple_smb.py
+++ qa/workunits/smb/tests/test_simple_smb.py
@@ -1,5 +1,6 @@
 import pytest
 
+import cephutil
 import smbutil
 
 
@@ -35,3 +36,48 @@ def test_create_file(smb_cfg, share_name):
             assert txt == 'HELLO WORLD\n'
         finally:
             fname.unlink()
+
+
+@pytest.mark.default
+def test_poke_mgr_module(smb_cfg):
+    jr = cephutil.cephadm_shell_cmd(
+        smb_cfg,
+        ['ceph', 'smb', 'show'],
+        load_json=True,
+    )
+    assert jr
+    clusters = [
+        r
+        for r in jr.obj['resources']
+        if r['resource_type'] == 'ceph.smb.cluster'
+    ]
+    assert len(clusters) == 1
+
+
+@pytest.mark.default
+def test_poke_mgr_module2(smb_cfg):
+    jr = cephutil.cephadm_shell_cmd(
+        smb_cfg,
+        ['ceph', 'smb', 'show', 'ceph.smb.share'],
+        load_json=True,
+    )
+    assert jr.obj
+    assert len(jr.obj['resources']) == 2
+    shares = [
+        r
+        for r in jr.obj['resources']
+        if r['resource_type'] == 'ceph.smb.share'
+    ]
+    assert len(shares) == 2
+
+    share = shares[0]
+    share['cluster_id'] = 'jimbo'
+    jr = cephutil.cephadm_shell_cmd(
+        smb_cfg,
+        ['ceph', 'smb', 'apply', '-i-'],
+        load_json=cephutil.LoadJSON.BOTH,
+        input_json={'resources': [share]},
+    )
+    assert jr.returncode != 0
+    assert jr.obj
+    assert not jr.obj.get('success')
